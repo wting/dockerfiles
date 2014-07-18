fe614523ef8d71718c5856efb66e38fa25375290
William Ting <io@williamting.com>
2014-07-18 07:54:56 -0700

    simplify apt-fast

--- base/apt-fast.sh
+++ base/apt-fast.sh
@@ -1,8 +1,19 @@
-#!/bin/sh
-if echo "$@" | grep -q "upgrade\|install\|dist-upgrade"; then
-    cd /var/cache/apt/archives/;
-    apt-get -y --print-uris $@ | egrep -o -e "(ht|f)tp://[^\']+" > apt-fast.list && cat apt-fast.list | xargs -l1 axel -a
-    aptitude $@;
-else
-    aptitude $@;
+#!/bin/bash
+[[ $(whoami) == "root" ]] || exec sudo "${0}" "${@}"
+
+if ! command -v aptitude &>/dev/null; then
+    apt-get install -y aptitude
 fi
+
+if ! command -v axel &>/dev/null; then
+    aptitude install axel -y
+fi
+
+if echo "${@}" | grep -q "upgrade\|install\|dist-upgrade\|full-upgrade"; then
+    apt-get --print-uris ${@} \
+        | grep -E -o -e "(ht|f)tp://[^\']+" \
+        | xargs --max-lines=1 axel --num-connections=2 --output=/var/cache/apt/archives/ -a \
+        || aptitude clean
+fi
+
+aptitude ${@};
