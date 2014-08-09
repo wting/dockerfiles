e5fed78904052bf7b9c2df1b6a08a49d4e949151
William Ting <io@williamting.com>
2014-08-09 16:23:09 -0700

    fix trailing slash

--- base/apt-fast.sh
+++ base/apt-fast.sh
@@ -12,7 +12,7 @@ fi
 if echo "${@}" | grep -q "upgrade\|install\|dist-upgrade\|full-upgrade"; then
     apt-get --print-uris ${@} \
         | grep -E -o -e "(ht|f)tp://[^\']+" \
-        | xargs --max-lines=1 axel --num-connections=2 --output=/var/cache/apt/archives/ -a \
+        | xargs --max-lines=1 axel --num-connections=2 --output=/var/cache/apt/archives -a \
         || aptitude clean
 fi
 
