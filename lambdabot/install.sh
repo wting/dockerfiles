282b0a77a3a6c06feb2d360ab0d44f1748e9fed4
William Ting <wting@yelp.com>
2014-08-09 21:38:26 -0700

    add incomplete lambdabot dockerfile

---
+++ lambdabot/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cabal install \
+    SafeSemaphore >=0.9 \
+    dependent-map >=0.1 \
+    dependent-sum-template >=0.0.0.1 \
+    edit-distance >=0.2 \
+    hslogger >=1.2.1 \
+    lifted-base >=0.2 \
+    monad-control >=0.3 \
+    random-fu >=0.2 \
+    random-source >=0.3 \
+    regex-tdfa >=1.1 \
+    transformers-base >=0.4 \
+    utf8-string >=0.3
